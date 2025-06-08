e24f786b75e8e50ba52737bd64342bd317ee52fd
ingcodeac <ingcodeac@gmail.com>
2025-06-09 01:54:22 +0900

    main.js_pokemon_selector

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const pokemon = Math.floor(Math.random()*1025)+1;
 pokemon = "number"
-document.getElementById("btn1").addEventListener('click');
+document.getElementById("btn1").addEventListener('click',pokemon);
 function pokemon(){
     alert("number");
 }
