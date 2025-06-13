9fab60268ac0642d8e3afcce3d2313908afc9711
ingcodeac <ingcodeac@gmail.com>
2025-06-13 23:29:59 +0900

    main.js_pokemon_selector_end

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function randomPokemonNumber() {
     const randomNumber = 
     Math.floor(Math.random()*1025)+1;
 
-
     const randomNumberElement =
     document.getElementById("random-number");
     randomNumberElement.innerText =
